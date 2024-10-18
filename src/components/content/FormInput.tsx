4006e87fc1245447abce33b70e626afa5e71ed4d
ngrwisnu <adiprayogongrwisnu@gmail.com>
2024-10-18 10:12:06 +0700

    handle the limits of character for title input field

--- src/components/content/FormInput.tsx
+++ src/components/content/FormInput.tsx
@@ -1,14 +1,42 @@
+import { ChangeEvent, useState } from "react";
 import Button from "../ui/Button";
 import FormControl from "../ui/FormControl";
 
 const FormInput = () => {
+  const [titleVal, setTitleVal] = useState<string>("");
+  const [bodyVal, setBodyVal] = useState<string>("");
+
+  const titleChangeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    if (e.target.value.length <= 50) {
+      setTitleVal(e.target.value);
+    }
+  };
+
+  const bodyChangeHandler = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setBodyVal(e.target.value);
+  };
+
   return (
     <form
       onSubmit={(e) => e.preventDefault()}
       className="bg-white rounded-lg basis-full max-w-[500px] mx-auto p-4 flex flex-col gap-4"
     >
-      <FormControl id="title" label="Title" />
-      <FormControl id="body" label="Description" />
+      <FormControl
+        id="title"
+        label="Title"
+        value={titleVal}
+        onChange={titleChangeHandler}
+      />
+      <FormControl
+        id="body"
+        label="Description"
+        value={bodyVal}
+        onChange={bodyChangeHandler}
+      />
       <div className="mt-4 flex justify-end">
         <Button type="primary" onClick={() => console.log("clicked")}>
           Add Note
--- src/components/ui/FormControl.tsx
+++ src/components/ui/FormControl.tsx
@@ -1,10 +1,30 @@
+import { ChangeEvent } from "react";
+
 interface FormControlProps {
   id: string;
   label: string;
   type?: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
 }
 
-const FormControl = ({ id, label, type = "text" }: FormControlProps) => {
+const FormControl = ({
+  id,
+  value,
+  label,
+  onChange,
+  type = "text",
+}: FormControlProps) => {
+  const charactersLimit = 50;
+
+  const changeColor = () => {
+    if (id === "title" && value.length > 40) {
+      return `text-red-500`;
+    } else {
+      return `text-neutral-500`;
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2 w-full">
       <div className="flex justify-between items-center">
@@ -12,14 +32,28 @@ const FormControl = ({ id, label, type = "text" }: FormControlProps) => {
           {label}
         </label>
         {id === "title" && (
-          <span className="text-neutral-500 text-sm">50 left</span>
+          <span className={`text-sm ${changeColor()}`}>
+            {charactersLimit - value.length} left
+          </span>
         )}
       </div>
-      <input
-        className="bg-blue-50 text-lg rounded-md p-2"
-        id={id}
-        type={type}
-      />
+      {id === "body" ? (
+        <textarea
+          id={id}
+          className="bg-blue-50 text-lg rounded-md p-2"
+          rows={5}
+          value={value}
+          onChange={onChange}
+        />
+      ) : (
+        <input
+          className="bg-blue-50 text-lg rounded-md p-2"
+          id={id}
+          type={type}
+          value={value}
+          onChange={onChange}
+        />
+      )}
     </div>
   );
 };
